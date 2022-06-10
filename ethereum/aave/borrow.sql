2e2634580beec2534fe041bcfecb18979bea6e98
JejuneMaxi <95482314+JejuneMaxi@users.noreply.github.com>
2022-06-10 10:20:00 +0200

    Updated changes

--- ethereum/aave/borrow.sql
+++ ethereum/aave/borrow.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS aave.aave_borrow (   
-   version text,
+CREATE TABLE IF NOT EXISTS aave.borrow (   
+    version text,
     transaction_type text,
     loan_type text,
     symbol text,
+    token bytea,
     contract_address bytea,
     borrower bytea,
     repayer bytea,
@@ -16,16 +17,17 @@ CREATE TABLE IF NOT EXISTS aave.aave_borrow (
     PRIMARY KEY (evt_tx_hash, evt_index)
 );
 
-CREATE OR REPLACE FUNCTION aave.insert_aave_borrow(start_ts timestamptz, end_ts timestamptz=now(), start_block numeric=0, end_block numeric=9e18) RETURNS integer
+CREATE OR REPLACE FUNCTION aave.insert_borrow(start_ts timestamptz, end_ts timestamptz=now(), start_block numeric=0, end_block numeric=9e18) RETURNS integer
 LANGUAGE plpgsql AS $function$
 DECLARE r integer;
 BEGIN
 WITH rows AS (
-    INSERT INTO aave.aave_borrow (
+    INSERT INTO aave.borrow (
       version,
       transaction_type,
       loan_type,
       symbol,
+      token,
       contract_address,
       borrower,
       repayer,
@@ -43,7 +45,8 @@ WITH rows AS (
       transaction_type,
       loan_type,
       erc20.symbol,
-      borrow.contract_address,
+      borrow.token,
+      erc20.contract_address,
       borrower,
       repayer,
       liquidator,
@@ -61,22 +64,22 @@ SELECT
     '1' AS version,
     'borrow' AS transaction_type,
     CASE 
-    WHEN "_borrowRateMode" = '1' THEN 'stable'
-    WHEN "_borrowRateMode" = '2' THEN 'variable'
+      WHEN "_borrowRateMode" = '1' THEN 'stable'
+      WHEN "_borrowRateMode" = '2' THEN 'variable'
     END AS loan_type,
     CASE
-    WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --Use WETH instead of Aave "mock" address
-    ELSE _reserve
-    END AS contract_address,
+      WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --Use WETH instead of Aave "mock" address
+      ELSE _reserve
+    END AS token,
     "_user" AS borrower,
-     NULL::bytea as repayer,
+    NULL::bytea AS repayer,
     NULL::bytea AS liquidator,
     _amount AS amount, 
     evt_tx_hash,
     evt_index,
     evt_block_time,
     evt_block_number
-FROM aave."LendingPool_evt_Borrow" tr1
+FROM aave."LendingPool_evt_Borrow" 
 UNION ALL 
 -- all repays
 SELECT 
@@ -84,9 +87,9 @@ SELECT
     'repay' AS transaction_type,
     NULL AS loan_type,
     CASE
-    WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --Use WETH instead of Aave "mock" address
-    ELSE _reserve
-    END AS contract_address,
+      WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' 
+      ELSE _reserve
+    END AS token,
     "_user" AS borrower,
     "_repayer" AS repayer,
     NULL::bytea AS liquidator,
@@ -99,13 +102,13 @@ FROM aave."LendingPool_evt_Repay"
 UNION ALL 
 -- liquidation
 SELECT 
-    '2' AS version,
+    '1' AS version,
     'borrow_liquidation' AS transaction_type,
     NULL AS loan_type,
     CASE
-    WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' --Use WETH instead of Aave "mock" address
-    ELSE _reserve
-    END AS contract_address,
+      WHEN _reserve = '\xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE' THEN '\xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' 
+      ELSE _reserve
+    END AS token,
     "_user" AS borrower,
     _liquidator AS repayer,
     _liquidator AS liquidator,
@@ -120,19 +123,19 @@ SELECT
     '2' AS version,
     'borrow' AS transaction_type,
     CASE 
-    WHEN "borrowRateMode" = '1' THEN 'stable'
-    WHEN "borrowRateMode" = '2' THEN 'variable'
+      WHEN "borrowRateMode" = '1' THEN 'stable'
+      WHEN "borrowRateMode" = '2' THEN 'variable'
     END AS loan_type,
-    reserve AS contract_address,
+    reserve AS token,
     "user" AS borrower, 
-     NULL::bytea as repayer,
+    NULL::bytea AS repayer,
     NULL::bytea AS liquidator,
     amount, 
     evt_tx_hash,
     evt_index,
     evt_block_time,
     evt_block_number
-FROM aave_v2."LendingPool_evt_Borrow" tr1
+FROM aave_v2."LendingPool_evt_Borrow" 
 UNION ALL 
 -- all repays
 SELECT 
@@ -184,18 +187,18 @@ RETURN r;
 END
 $function$;
 
-SELECT aave.insert_aave_borrow(DATE_TRUNC('day','2019-01-24'::timestamptz),DATE_TRUNC('day',NOW()) )
+SELECT aave.insert_borrow(DATE_TRUNC('day','2019-01-24'::timestamptz),DATE_TRUNC('day',NOW()) )
 WHERE NOT EXISTS (
     SELECT *
-    FROM aave.aave_borrow
+    FROM aave.borrow
 );
 
 INSERT INTO cron.job (schedule, command)
 VALUES ('*/20 * * * *', $$
-    SELECT aave.insert_aave_borrow(
-        (SELECT MAX(evt_block_time) - interval '1 days' FROM aave.aave_borrow),
+    SELECT aave.insert_borrow(
+        (SELECT MAX(evt_block_time) - interval '1 days' FROM aave.borrow),
         (SELECT now() - interval '20 minutes'),
-        (SELECT MAX(number) FROM ethereum.blocks WHERE time < (SELECT MAX(evt_block_time) - interval '1 days' FROM aave.aave_borrow)),
+        (SELECT MAX(number) FROM ethereum.blocks WHERE time < (SELECT MAX(evt_block_time) - interval '1 days' FROM aave.borrow)),
         (SELECT MAX(number) FROM ethereum.blocks where time < now() - interval '20 minutes'));
 $$)
 ON CONFLICT (command) DO UPDATE SET schedule=EXCLUDED.schedule;
